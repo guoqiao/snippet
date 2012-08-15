6c7d36beed73b687f8138e12158faae029d80ce8
guoqiao <guoqiao@gmail.com>
2012-08-15 21:01:17 +0800

    add model Snippet

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Snippet(models.Model):
+    code = models.TextField()
+    desc = models.CharField(max_length=512)
+    user = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
