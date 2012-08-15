0e8e159f96b3fd8bf7ab62e04bff73a85384f8f8
guoqiao <guoqiao@gmail.com>
2012-08-15 17:30:53 +0800

    rename url home to index in app accounts

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('accounts.views',
-    url(r'^$', 'home', name='home'),
+    url(r'^$', 'index', name='index'),
     url(r'^logout/$', 'logout_user', name='logout'),
     url(r'^register/$', 'register', name='register'),
 )
