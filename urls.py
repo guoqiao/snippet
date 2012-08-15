282b20648cdbf51f58ffe8ce5fb5fd7a635d4ce7
guoqiao <guoqiao@gmail.com>
2012-08-15 22:46:54 +0800

    add url conf for static and media

--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,3 +19,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
