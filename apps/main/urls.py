e4421f329a02434f6c9fdf3da5eb8e46ee0205da
guoqiao <guoqiao@gmail.com>
2012-08-16 17:07:42 +0800

    add detail view and it works

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('main.views',
     url(r'^$', 'index', name='index'),
+    url(r'^(?P<pk>\d+)/$', 'detail', name='detail'),
 )
 
--- apps/main/views.py
+++ apps/main/views.py
@@ -5,7 +5,14 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import render,redirect
 
+from .models import Snippet
+
 def index(request):
-    ctx = {}
+    objs = Snippet.objects.all()
+    ctx = {'objs': objs}
     return render(request, 'main/index.html', ctx)
 
+def detail(request, pk):
+    obj = Snippet.objects.get(pk=pk)
+    ctx = {'obj': obj}
+    return render(request, 'main/detail.html', ctx)
